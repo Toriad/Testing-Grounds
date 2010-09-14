a46d98b3accacdebe9f90a54613a2ab417ff7c77
U-DEADHOUSE\james.black <james.black@DEAD-PER-PC-01.deadhouse.local>
2010-09-14 18:05:49 +0800

    Base Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestingGrounds::Application.config.secret_token = 'fad43eaa0b54619b37a0fbd29530406cb3a6038e73fc09a7a9c2d7b8fbfb107d2a19bcffcb7fdeb3a9a1cf6eb62f45b78faea8a8ab82a8f5b2a2028c4a41dae2'
